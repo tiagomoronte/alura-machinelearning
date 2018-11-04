b063bf3c47bd08d244ea05cd9f38fe1b061cce23
tiagomoronte <tiago.moronte@gmail.com>
2018-11-04 17:41:04 -0200

    Taxa de acerto base.

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from collections import Counter
 
 df = pd.read_csv('busca.csv')
 X_df = df[['home','busca','logado']]
@@ -7,5 +9,29 @@ Y_df = df['comprou']
 X = pd.get_dummies(X_df).values
 Y = Y_df.values
 
-print(X,Y)
+porcentagem_de_treino = 0.9
+tamanho_de_treino = int(porcentagem_de_treino * len(Y))
+tamanho_teste = int(len(Y) - tamanho_de_treino)
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+teste_dados = X[-tamanho_teste:]
+teste_marcacoes = Y[-tamanho_teste:]
+
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+acertos = resultado == teste_marcacoes
+total_de_acertos = sum(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_acertos = total_de_acertos / total_de_elementos
+
+taxa_acerto_base = max(Counter(teste_marcacoes).values()) / len(Y)
+
+print(taxa_acerto_base)
+print(taxa_acertos)
 
